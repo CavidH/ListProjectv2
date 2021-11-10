8295c64d026efcdb2bc4230eac5e646f063c6f6a
CavidH <hcavid386@gmail.com>
2021-11-10 10:54:48 +0400

    fix

--- ListLib/MyList.cs
+++ ListLib/MyList.cs
@@ -1,31 +1,148 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 
 namespace ListLib
 {
     public class MyList<T>
     {
+         
         private T[] _data;
-        private int _index;
+        private int _index = 0;
         private int _capacityIncLvl = 2;//her defe yeni list yaradilinda capacity  3 defe artacaq
-        public T this[int index] { get { return _data[index]; } set { _data[index] = value; } }
+        #region Indexer
+        public T this[int index]
+        {
+            get { return _data[index]; }
+            set { _data[index] = value; }
+        }
+        #endregion
+        #region Constructor
         public MyList()
         {
-
             _data = new T[1];
-            _index = 0;
         }
+        public MyList(int capacity)
+        {
+            _data = new T[capacity];
+        }
+        #endregion
+        #region Add
         public void Add(T item)
         {
-            if (_index==_data.Length)
+            if (_index == _data.Length)
             {
                 ListCreator();
             }
             _data[_index] = item;
             _index++;
         }
+        #endregion
+        #region AddRange
+        public void AddRange(T[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Add(arr[i]);
+            }
+        }
+        #endregion
+        #region Clear
+        public void Clear()
+        {
+            _data = _data = new T[0];
+        }
+        #endregion
+        #region ListCreator
         private void ListCreator()
         {
+            T[] NewList = new T[_data.Length * _capacityIncLvl];
+            _data = ListCopyist(NewList, _data);// yeni gelen list dataya menimsedildi
+        }
+        #endregion
+        #region ListCopyist
+        private T[] ListCopyist(T[] NewList, T[] _data)
+        {
+            for (int i = 0; i < _data.Length; i++)
+            {
+                NewList[i] = _data[i];
+            }
+            return NewList;
+        }
+        #endregion
+        #region IndexOf
+        public int IndexOf(int item)
+        {
+            for (int i = 0; i < _data.Length; i++)
+            {
+                if (item.Equals(_data[i]))
+                {
+                    return i;
+                }
 
+            }
+            return -1;
         }
+        public int IndexOf(int item,int index)
+        {
+            if (item.Equals(_data[index]))
+            {
+                return index;
+            }
+            return -1;
         }
+        #endregion
+        #region LastIndexOf
+        public int LastIndexOf(T item)
+        {
+            for (int i = _data.Length - 1; i >= 0; i--)
+            {
+                if (item.Equals(_data[i]))
+                {
+                    return i;
+                }
+
+            }
+            return -1;
+        }
+        public int LastIndexOf(int item, int index)
+        {
+            if (item.Equals(_data[index]))
+            {
+                return index;
+            }
+            return -1;
+        }
+        #endregion
+        #region Remove
+        public void Remove(T item)
+        {
+            for (int i = 0; i < _data.Length; i++)
+            {
+                if (item.Equals(_data[i]))
+                {
+                    _data[i] = default;
+                }
+            }
+        }
+        public void RemoveAt(int index)
+        {
+            _data[index] = default;
+        }
+        #endregion
+        #region Reverse
+
+        #endregion
+        #region Length
+        public int GetLength()
+        {
+            return _index;
+        }
+        #endregion
+
+
+
+
+    }
 }
--- ListProject/Program.cs
+++ ListProject/Program.cs
@@ -8,10 +8,38 @@ namespace ListProject
     {
         static void Main(string[] args)
         {
-           // MyList list = new MyList();
-            
-             
-            Console.WriteLine("Hello World!");
+            MyList<int> list = new MyList<int>();
+            list.Add(1);
+            list.Add(2); 
+            list.Add(3);
+            list.Add(3);
+            list.Add(4);
+            list.Add(5);
+            list.Add(6);
+            list.Add(4);
+            list.Add(4);
+            list.RemoveAt(5);
+            Console.WriteLine(list[5]);
+            List<int> k = new List<int>();
+            k.Add(4);
+            k.Add(5);
+            k.Add(6);
+            k.Add(3);
+            k.Add(3);
+            k.Add(1);
+            k.Remove(6);
+
+            for (int i = 0; i < k.Count; i++)
+            {
+                Console.WriteLine(k[i]);
+            }/**
+
+            Console.WriteLine( k.LastIndexOf(3) );
+
+
+
+
+            Console.WriteLine("Hello World!");**/
         }
     }
 }
