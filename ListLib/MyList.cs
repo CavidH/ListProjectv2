1919e95a64466cb924bedae287262297c94df5f3
CavidH <hcavid386@gmail.com>
2021-11-10 08:37:18 +0400

    Add project files.

---
+++ ListLib/MyList.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ListLib
+{
+    public class MyList<T>
+    {
+        private T[] _data;
+        private int _index;
+        private int _capacityIncLvl = 2;//her defe yeni list yaradilinda capacity  3 defe artacaq
+        public T this[int index] { get { return _data[index]; } set { _data[index] = value; } }
+        public MyList()
+        {
+
+            _data = new T[1];
+            _index = 0;
+        }
+        public void Add(T item)
+        {
+            if (_index==_data.Length)
+            {
+                ListCreator();
+            }
+            _data[_index] = item;
+            _index++;
+        }
+        private void ListCreator()
+        {
+
+        }
+        }
+}
---
+++ ListProject/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using ListLib;
+
+namespace ListProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // MyList list = new MyList();
+            
+             
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
