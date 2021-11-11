838bed796d0887a5f603c3b9b7381577f9795f72
Cavid <hcavid386@gmail.com>
2021-11-11 13:48:28 +0400

    Update Program.cs

--- ListProject/Program.cs
+++ ListProject/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using ListLib;
 
@@ -8,38 +8,71 @@ namespace ListProject
     {
         static void Main(string[] args)
         {
+            /**
+            List<int> l = new List<int>();
+            l.Add(4);
+            l.Add(5);
+            l.Add(3);
+            l.Add(8);
+            l.Add(6);
+            **/
+          //  Console.WriteLine(l.FindAll(n => n > 3));
+
+
+            Console.WriteLine("Custom listimiz");
+
             MyList<int> list = new MyList<int>();
-            list.Add(1);
-            list.Add(2); 
-            list.Add(3);
-            list.Add(3);
             list.Add(4);
+            list.Add(5);
+            list.Add(3);
+            list.Add(8);
+            list.Add(6);
             list.Add(5);
             list.Add(6);
             list.Add(4);
             list.Add(4);
-        
-         
-
-            for (int i = 0; i < list.GetLength(); i++)
-            {
-                Console.WriteLine(list[i]);
-            }
-            Console.WriteLine("--------------");
-            list.Reverse();
+            list.Remove(4);
             for (int i = 0; i < list.GetLength(); i++)
             {
                 Console.WriteLine(list[i]);
             }
 
             /**
+           // Console.WriteLine(list.Find(n => n>3));
+            MyList<int> myList=list.FindAll(n => n%2==0);
+            for (int i = 0; i < myList.GetLength(); i++)
+            {
+                Console.WriteLine(myList[i]);
+            }
 
-            Console.WriteLine( k.LastIndexOf(3) );
+
+
+            }
+              
+            **/
+
+
+            /**
+
+               for (int i = 0; i < list.GetLength(); i++)
+               {
+                   Console.WriteLine(list[i]);
+               }
+               Console.WriteLine("--------------");
+               list.Reverse();
+               for (int i = 0; i < list.GetLength(); i++)
+               {
+                   Console.WriteLine(list[i]);
+               }
+
+               
+
+               Console.WriteLine( k.LastIndexOf(3) );
 
 
 
 
-            Console.WriteLine("Hello World!");**/
+               Console.WriteLine("Hello World!");**/
         }
     }
 }
